0f29bf7c8bb747d1012ead5be7bf44baf56c92d3
Oscar Barrera <osjamfer@hotmail.com>
2024-04-25 00:02:08 +0200

    add front

--- models/Author.js
+++ models/Author.js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const allowedCountries = ["COLOMBIA", "RUSSIA", "ENGLAND", "UNITED STATES", "ARGENTINA", "CZECHOSLOVAKIA", "JAPAN", "NIGERIA"]
+
 // Creamos el schema del autor
 const authorSchema = new Schema(
   {
     name: {
       type: String,
       required: true,
+      minLength: [3, "por favor, el nombre de autor debe tener al menos 3 letras"],
+      maxLength: [20, "por favor, el nombre de autor debe tener al máximo 20 letras"],
+      trim: true,
     },
     country: {
       type: String,
       required: true,
+      uppercase: true,
+      enum: allowedCountries,
+      trim: true,
     },
   },
   {
--- models/Book.js
+++ models/Book.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const allowedCountries = ["COLOMBIA", "RUSSIA", "ENGLAND", "UNITED STATES", "ARGENTINA", "CZECHOSLOVAKIA", "JAPAN", "NIGERIA"]
+
 // Creamos el schema del libro
 const bookSchema = new Schema(
   {
     title: {
       type: String,
       required: true,
+      minLength: [3, "por favor, el titulo del libro debe tener al menos 3 letras"],
+      maxLength: [20, "por favor, el titulo del libro debe tener al máximo 20 letras"],
+      trim: true,
     },
     author: {
       type: mongoose.Schema.Types.ObjectId,
@@ -16,16 +21,24 @@ const bookSchema = new Schema(
     pages: {
       type: Number,
       required: false,
+      min: [1, "El número minimo es 1 pagina"],
+      max: [1000, "El número maximo es 1000 paginas"],
     },
     publisher: {
       type: {
         name: {
           type: String,
           required: true,
+          minLength: [3, "por favor, el nombre de la editorial debe tener al menos 3 letras"],
+          maxLength: [20, "por favor, el nombre de la editorial debe tener al máximo 20 letras"],
+          trim: true,
         },
         country: {
           type: String,
           required: true,
+          enum: allowedCountries,
+          uppercase: true,
+          trim: true,
         },
       },
       required: true,
